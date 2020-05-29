230de979241bb0c83e63684e0b6a5de3e8f902f3
Tomasz Targoński <tomasz.targonski95@gmail.com>
2020-05-29 12:40:55 +0200

    Updates

---
+++ precache-manifest.420276e00336d18964729843bf8afbe1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a39e1e9b721f0ecb6442bc2c65038e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f19be62b39107afa194",
+    "url": "/static/css/main.ffbbf822.chunk.css"
+  },
+  {
+    "revision": "04f7303258ac71793e69",
+    "url": "/static/js/2.5f851811.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5f851811.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f19be62b39107afa194",
+    "url": "/static/js/main.88bda691.chunk.js"
+  },
+  {
+    "revision": "6153d1b4e1d5f40969fb",
+    "url": "/static/js/runtime-main.af6f0f8f.js"
+  },
+  {
+    "revision": "999f4b0670fb7b0e40965bfc843332bb",
+    "url": "/static/media/wave.999f4b06.png"
+  }
+]);
