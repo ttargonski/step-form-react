aa6ceebd73230e9e3b475ced989beb3ff637ac71
Tomasz Targoński <tomasz.targonski95@gmail.com>
2020-05-29 12:49:11 +0200

    Updates

---
+++ precache-manifest.1243412e2f0f9423c71eb0f161a6bbfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "558c761bc8ddd508ac47472c52a7ca85",
+    "url": "/step-form-react/index.html"
+  },
+  {
+    "revision": "660a26a0d4ca8cf10dea",
+    "url": "/step-form-react/static/css/main.3a5e1750.chunk.css"
+  },
+  {
+    "revision": "04f7303258ac71793e69",
+    "url": "/step-form-react/static/js/2.5f851811.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/step-form-react/static/js/2.5f851811.chunk.js.LICENSE"
+  },
+  {
+    "revision": "660a26a0d4ca8cf10dea",
+    "url": "/step-form-react/static/js/main.6a7053cb.chunk.js"
+  },
+  {
+    "revision": "600231e7d2abacd2396c",
+    "url": "/step-form-react/static/js/runtime-main.17126851.js"
+  },
+  {
+    "revision": "999f4b0670fb7b0e40965bfc843332bb",
+    "url": "/step-form-react/static/media/wave.999f4b06.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.420276e00336d18964729843bf8afbe1.js"
+  "/step-form-react/precache-manifest.1243412e2f0f9423c71eb0f161a6bbfe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/step-form-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
